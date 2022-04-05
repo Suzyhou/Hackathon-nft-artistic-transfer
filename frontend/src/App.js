9c848f52f0b4b22abfa48aae0216e83c33ee6d78
Yufen <susie129225@gmail.com>
2022-04-05 16:35:10 -0400

    fixed upload, added buttons

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,5 @@
 import {useState, useEffect} from "react"
-import { ChakraProvider,Center, VStack, HStack, Heading,Text, Button} from "@chakra-ui/react"
+import { ChakraProvider,Center, VStack, HStack, Heading,Text, Button, SimpleGrid,Image} from "@chakra-ui/react"
 
 function App() {
     const [selectedFile, setSelectedFile] = useState(null)
@@ -8,11 +8,12 @@ function App() {
     
     const onInputChange = (e) => {
         setIsSelected(true)
-        setSelectedFile(e.target.file[0])
+        setSelectedFile(e.target.files[0])
     }
     const onFileUpload = (e) => {
         const formData = new FormData()
-        formData.append('file', selectedFile, selectedFile.name)
+        console.log(selectedFile)
+        formData.append('file', selectedFile, selectedFile.image)
         fetch("http://127.0.0.1:8000/upload",{
             method:'POST',
             body: formData
@@ -28,7 +29,7 @@ function App() {
                 <VStack spacing={7}>
                     <Heading>Your NFT Image Generator</Heading>
                     <Text>Upload your image here</Text>
-                    <HStack>
+                    <HStack> 
                         <input 
                             type='file' 
                             onChange={onInputChange}>
@@ -41,8 +42,35 @@ function App() {
                                 Upload Photo
                         </Button>
                     </HStack>
+                    <Text>Choose the artistic style</Text>
+                    <HStack>
+                        <SimpleGrid columns={3} spacing={8}>
+                            <VStack>
+                                <Image borderRadius={25} boxSize="300px" src="images/starrynight.png"></Image>
+                                <Button colorScheme="blue">Select</Button>
+                            </VStack>
+                            <VStack>
+                                <Image borderRadius={25} boxSize="300px" src="images/pixelart.png"></Image>
+                                <Button colorScheme="blue">Select</Button>
+                            </VStack>
+                            <VStack>
+                                <Image borderRadius={25} boxSize="300px" src="images/abstract.png"></Image>
+                                <Button colorScheme="blue">Select</Button>
+                            </VStack>
+                        </SimpleGrid>
+                    </HStack>
+                    <Text>or upload the style</Text>
+                    <HStack>
+                        <input 
+                            type='file' 
+                            onChange={onInputChange}>
+                        </input>
+                        <Button colorScheme="blue" isDisabled={!isSelected} >Upload your style</Button>
+                    </HStack>
+                    <Heading>Mint Your NFT</Heading>
+                    <Button colorScheme="blue">Mint</Button>
                 </VStack>
-            </Center>
+            </Center> 
         </ChakraProvider>
     )
 }
